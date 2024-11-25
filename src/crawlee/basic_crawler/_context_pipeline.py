e403c2ab4daa1427eaa6cfa0c0b317ebdaf071a0
Josef Prochazka <josef.prochazka@apify.com>
2024-11-25 16:10:50 +0100

    Fix last test

--- src/crawlee/basic_crawler/_context_pipeline.py
+++ src/crawlee/basic_crawler/_context_pipeline.py
@@ -64,33 +64,6 @@ class ContextPipeline(Generic[TCrawlingContext]):
             await self._run_middleware_cleanup(root_crawling_context)
 
     async def _run_middleware_action(self, root_crawling_context: BasicCrawlingContext) ->  tuple[TCrawlingContext, Any]:
-        """Middleware is expected to return generator of 1 or 2 items. First is used to run, second to cleanup. More is ignored.
-        That is too implicit and complex.
-
-        TODO: Redo middleware to accept into compose/init MiddlewareCallback class, that has .run and .cleanup method to be explicit.
-        -> Looks like natural use-case for context manager run = __aenter__, cleanup=__aexit__
-        which would require to remember children and not parent
-        natural order is then automatic and running pipeline is super simple. No need to reverse anything.
-        root-action -> children-action1 -> children2-action -> user_action -> children2->cleanup -> children1->cleanup -> root-cleanup
-
-        async def __call__(input_context, user_action):
-            with self(input_context, user_action):
-                print("Finished middleware and user call back. Start running cleanup.")
-
-        async def __aenter__(input_context, user_action):
-            self.context = await self.middleware.action()
-                if self.child:
-                    async with self.child(self.context)
-                else:
-                    await user_action(self.context)
-
-        async def __aexit__(...):
-            await self.middleware.cleanup()
-
-        top level call in basic_crawler:
-        async def __run_request_handler(self, context: BasicCrawlingContext) -> None:
-            self._context_pipeline(context, self.router)
-        """
         if self._parent is not None:
             parent_crawling_context = await self._parent._run_middleware_action(root_crawling_context=root_crawling_context)
         else:
@@ -107,7 +80,7 @@ class ContextPipeline(Generic[TCrawlingContext]):
         except ContextPipelineInterruptedError:
             raise
         except Exception as e:
-            raise ContextPipelineInitializationError(e, crawling_context) from e
+            raise ContextPipelineInitializationError(e, root_crawling_context) from e
 
     async def _run_middleware_cleanup(self, root_context):
         if self._middleware is not None:
@@ -123,55 +96,6 @@ class ContextPipeline(Generic[TCrawlingContext]):
                 raise RuntimeError('The middleware yielded more than once')
             await self._parent._run_middleware_cleanup(root_context)
 
-
-    async def __call__Old(
-        self,
-        crawling_context: BasicCrawlingContext,
-        final_context_consumer: Callable[[TCrawlingContext], Awaitable[None]],
-    ) -> None:
-        """Run a crawling context through the middleware chain and pipe it into a consumer function.
-
-        Exceptions from the consumer function are wrapped together with the final crawling context.
-        """
-        chain = list(self._middleware_chain())
-        cleanup_stack = list[AsyncGenerator]()
-
-        try:
-            for member in reversed(chain):
-                if member._middleware:  # noqa: SLF001
-                    middleware_instance = member._middleware(crawling_context)  # noqa: SLF001
-                    try:
-                        result = await middleware_instance.__anext__()
-                    except SessionError:  # Session errors get special treatment
-                        raise
-                    except StopAsyncIteration as e:
-                        raise RuntimeError('The middleware did not yield') from e
-                    except ContextPipelineInterruptedError:
-                        raise
-                    except Exception as e:
-                        raise ContextPipelineInitializationError(e, crawling_context) from e
-
-                    crawling_context = result
-                    cleanup_stack.append(middleware_instance)
-
-            try:
-                await final_context_consumer(cast(TCrawlingContext, crawling_context))
-            except SessionError:  # Session errors get special treatment
-                raise
-            except Exception as e:
-                raise RequestHandlerError(e, crawling_context) from e
-        finally:
-            for middleware_instance in reversed(cleanup_stack):
-                try:
-                    result = await middleware_instance.__anext__()
-                except StopAsyncIteration:  # noqa: PERF203
-                    pass
-                except ContextPipelineInterruptedError as e:
-                    raise RuntimeError('Invalid state - pipeline interrupted in the finalization step') from e
-                except Exception as e:
-                    raise ContextPipelineFinalizationError(e, crawling_context) from e
-                else:
-                    raise RuntimeError('The middleware yielded more than once')
 
     def compose(
         self: type[TParentCrawlingContext],
